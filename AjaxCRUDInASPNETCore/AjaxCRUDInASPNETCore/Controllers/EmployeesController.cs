64fbfc6b76aa92b2859a2e4aaace2218549f82e2
Diogo Freitas <diogo@vdata.com.br>
2020-06-09 15:06:31 -0300

    Popup CRUD Created

--- AjaxCRUDInASPNETCore/AjaxCRUDInASPNETCore/Controllers/EmployeesController.cs
+++ AjaxCRUDInASPNETCore/AjaxCRUDInASPNETCore/Controllers/EmployeesController.cs
@@ -24,115 +24,63 @@ namespace AjaxCRUDInASPNETCore.Controllers
             return View(await _context.Employees.ToListAsync());
         }
 
-        // GET: Employees/Details/5
-        public async Task<IActionResult> Details(int? id)
+        // GET: Employees/AddOrEdit
+        public async Task<IActionResult> AddOrEdit(int id = 0)
         {
-            if (id == null)
+            if (id == 0)
             {
-                return NotFound();
+                return View(new Employees());
+            }
+            else
+            {
+                var employees = await _context.Employees.FindAsync(id);
+                if (employees == null)
+                {
+                    return NotFound();
+                }
+                return View(employees);
             }
 
-            var employees = await _context.Employees
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (employees == null)
-            {
-                return NotFound();
-            }
-
-            return View(employees);
         }
-
-        // GET: Employees/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Employees/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,Departament,Salary,Email")] Employees employees)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(employees);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(employees);
-        }
-
-        // GET: Employees/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var employees = await _context.Employees.FindAsync(id);
-            if (employees == null)
-            {
-                return NotFound();
-            }
-            return View(employees);
-        }
-
         // POST: Employees/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Departament,Salary,Email")] Employees employees)
+        public async Task<IActionResult> AddOrEdit(int id, [Bind("Id,FirstName,LastName,Departament,Salary,Email,Date")] Employees employees)
         {
-            if (id != employees.Id)
-            {
-                return NotFound();
-            }
-
             if (ModelState.IsValid)
             {
-                try
+                if (id == 0)
                 {
-                    _context.Update(employees);
+                    employees.Date = DateTime.Now;
+                    _context.Add(employees);
                     await _context.SaveChangesAsync();
                 }
-                catch (DbUpdateConcurrencyException)
+                else
                 {
-                    if (!EmployeesExists(employees.Id))
+                    try
                     {
-                        return NotFound();
+                        _context.Update(employees);
+                        await _context.SaveChangesAsync();
                     }
-                    else
+                    catch (DbUpdateConcurrencyException)
                     {
-                        throw;
+                        if (!EmployeesExists(employees.Id))
+                        {
+                            return NotFound();
+                        }
+                        else
+                        {
+                            throw;
+                        }
                     }
                 }
-                return RedirectToAction(nameof(Index));
+
+                return Json(new { isValid = true, html = Helper.RenderRazorViewToString(this, "_ViewAll", _context.Employees.ToList()) });
             }
-            return View(employees);
+            return Json(new { isValid = false, html = Helper.RenderRazorViewToString(this, "AddOrEdit", employees)});
         }
-
-        // GET: Employees/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var employees = await _context.Employees
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (employees == null)
-            {
-                return NotFound();
-            }
-
-            return View(employees);
-        }
-
         // POST: Employees/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
@@ -141,7 +89,7 @@ namespace AjaxCRUDInASPNETCore.Controllers
             var employees = await _context.Employees.FindAsync(id);
             _context.Employees.Remove(employees);
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            return Json(new {html = Helper.RenderRazorViewToString(this, "_ViewAll", _context.Employees.ToList()) });
         }
 
         private bool EmployeesExists(int id)
--- AjaxCRUDInASPNETCore/AjaxCRUDInASPNETCore/Models/Employees.cs
+++ AjaxCRUDInASPNETCore/AjaxCRUDInASPNETCore/Models/Employees.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -11,15 +12,29 @@ namespace AjaxCRUDInASPNETCore.Models
     {
         [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "This Field is required")]
+        [DisplayName("Fist Name")]
         [Column(TypeName = "nvarchar(40)")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "This Field is required")]
+        [DisplayName("Last Name")]
         [Column(TypeName = "nvarchar(40)")]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "This Field is required")]
         [Column(TypeName = "nvarchar(60)")]
         public string Departament { get; set; }
+
+        [Required(ErrorMessage = "This Field is required")]
         public double Salary { get; set; }
+
+        [Required(ErrorMessage = "This Field is required")]
         [Column(TypeName = "nvarchar(60)")]
         public string Email { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
     }
 }
